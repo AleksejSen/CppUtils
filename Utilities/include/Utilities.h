afb5a4798309d2e3380ed2d54f4f2426f1852c70
Alekjsejs Sencenko <aleksejs.sencenko@consiliumsafety.com>
2023-06-02 16:50:47 +0200

    Add:Utilities.h fix

--- Utilities/include/Utilities.h
+++ Utilities/include/Utilities.h
@@ -1,6 +1,7 @@
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 #include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -12,6 +13,15 @@ public:
   ReadFileToStringVector(std::string const &filePath);
 
   static std::string ReadFileToString(std::string const &filePath);
+
+  static std::vector<std::string>
+  SplitStringByDelimiter(std::string const &inputStr, std::string delimiter);
+
+  static std::string GetStringBetweenTwoChar(std::string const &inputStr,
+                                             const char startChar,
+                                             const char endChar);
+  static std::vector<std::string>
+  ExtractStringPatternRegex(std::string const &inputStr, std::regex pattern);
 };
 
 #endif
